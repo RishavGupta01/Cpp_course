46eed841fb262013dc1a20f201e95ca8db41ef1f
RishavGupta01 <110810862+RishavGupta01@users.noreply.github.com>
2024-02-28 11:30:27 +0530

    Some basic tutorials for C++

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+// we can use the above line to include the iostream library
+int main(){
+    std::cout<<"Hello World\n";// this is how we print something in c++
+    return 0;
+}
+//we can also use"using namespace std;" to avoid using std::cout and std::cin
+
+// using namespace std;
+// int main(){
+//     cout<<"Hello World\n";
+//     return 0;
+// }
---
+++ second.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+//this is created by rishav gupta
+//this is a single line comment
+/*this is a 
+multi-line comment*/
+int main(){
+    int x = 5; //these are variables
+    int y = 6;
+    cout<<x+y<<"Hello World\n";
+}
